a6050ee7a3a294695b82ca4a34f017acc4c4b868
Nirex0 <Nirex.0@gmail.com>
2018-07-28 00:31:09 +0430

    [NLib] Add: Main include header (Contains everything)

---
+++ src/NCore/NLib.h
@@ -0,0 +1,11 @@
+// © 2018 NIREX ALL RIGHTS RESERVED
+
+#ifndef _N_LIB_H_
+#define _N_LIB_H_
+
+#include "NDefines.h"
+#include "NObject.h"
+#include "NEventArgs.h"
+#include "NRegistry.h"
+
+#endif // !_N_LIB_H_
