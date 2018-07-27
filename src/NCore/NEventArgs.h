842d5a7ae1d5298f940f4bc959b1eba1319fe49f
Nirex0 <Nirex.0@gmail.com>
2018-07-28 00:30:11 +0430

    [NLib] Add: EventArgs class

---
+++ src/NCore/NEventArgs.h
@@ -0,0 +1,13 @@
+// © 2018 NIREX ALL RIGHTS RESERVED
+
+#ifndef _N_EVENT_ARGS_H_
+#define _N_EVENT_ARGS_H_
+
+#include "NObject.h"
+
+namespace NLib
+{
+	class NEventArgs : public NObject {};
+}
+#endif // !_N_EVENT_ARGS_H_
+
