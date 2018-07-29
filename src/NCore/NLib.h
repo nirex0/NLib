cbd66947dec6253aa7e62ddf1aad229ccd8586e6
Nirex0 <Nirex.0@gmail.com>
2018-07-29 13:55:39 +0430

    [NLib] Update: NLib.h

--- src/NCore/NLib.h
+++ src/NCore/NLib.h
@@ -4,8 +4,12 @@
 #define _N_LIB_H_
 
 #include "NDefines.h"
+#include "NWindows.h"
 #include "NObject.h"
 #include "NEventArgs.h"
 #include "NRegistry.h"
+#include "NGenerator.h"
+#include "NGeneratorArgs.h"
+#include "NLogicalBound.h"
 
 #endif // !_N_LIB_H_
