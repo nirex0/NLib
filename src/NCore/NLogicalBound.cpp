ecaaee5f988d5764555e34136defb1cfd0dce82d
Nirex0 <Nirex.0@gmail.com>
2018-07-29 13:55:00 +0430

    [NLib] Add: NLogicalBound class

---
+++ src/NCore/NLogicalBound.cpp
@@ -0,0 +1,61 @@
+// © 2018 NIREX ALL RIGHTS RESERVED
+
+#include "NLogicalBound.h"
+
+namespace NLib
+{
+	NLogicalBound::NLogicalBound()
+	{
+	}
+
+	NLogicalBound::~NLogicalBound()
+	{
+	}
+
+	bool NLogicalBound::Binding(void) const
+	{
+		return m_binding;
+	}
+
+	bool NLogicalBound::Binding(bool intake)
+	{
+		m_binding = intake;
+		return m_binding;
+	}
+
+	NBinding* NLogicalBound::Function(void) const
+	{
+		return m_annex;
+	}
+
+	NBinding* NLogicalBound::Function(NBinding intake)
+	{
+		m_annex = &intake;
+		return m_annex;
+	}
+
+	void NLogicalBound::operator=(NBinding rhs)
+	{
+		m_annex = &rhs;
+	}
+
+	void NLogicalBound::operator()(void)
+	{
+		NBinding current;
+		if (m_annex)
+		{
+			current = *m_annex;
+			current(m_binding);
+		}
+	}
+
+	void NLogicalBound::Run(void)
+	{
+		NBinding current;
+		if (m_annex)
+		{
+			current = *m_annex;
+			current(m_binding);
+		}
+	}
+}
