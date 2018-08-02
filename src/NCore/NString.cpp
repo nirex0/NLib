de428158efcdea1acfa8ceec3924bb53a7a7abf7
Nirex0 <Nirex.0@gmail.com>
2018-08-03 00:25:13 +0430

    [NLib] Add: NString utility class

---
+++ src/NCore/NString.cpp
@@ -0,0 +1,202 @@
+// © 2018 NIREX ALL RIGHTS RESERVED
+
+#include "NString.h"
+#include <locale>
+#include <codecvt>
+
+namespace NLib
+{
+	char* NString::ToUpper(char* temp)
+	{
+		char* retc = temp;
+
+		int i = 0;
+		while (retc[i])
+		{
+			retc[i] = toupper(retc[i]);
+			i++;
+		}
+		return retc;
+	}
+
+	char* NString::ToLower(char* temp)
+	{
+		char* retc = temp;
+
+		int i = 0;
+		while (retc[i])
+		{
+			retc[i] = tolower(retc[i]);
+			i++;
+		}
+		return retc;
+	}
+
+	char* NString::Proper(char* temp)
+	{
+		char* retc = temp;
+
+		int i = 0;
+		while (retc[i])
+		{
+			retc[i] = tolower(retc[i]);
+			i++;
+		}
+		retc[0] = toupper(retc[0]);
+		return retc;
+	}
+
+	wchar_t* NString::ToUpper(wchar_t* temp)
+	{
+		wchar_t* retc = temp;
+
+		int i = 0;
+		while (retc[i])
+		{
+			retc[i] = toupper(retc[i]);
+			i++;
+		}
+		return retc;
+	}
+
+	wchar_t* NString::ToLower(wchar_t* temp)
+	{
+		wchar_t* retc = temp;
+
+		int i = 0;
+		while (retc[i])
+		{
+			retc[i] = tolower(retc[i]);
+			i++;
+		}
+		return retc;
+	}
+
+	wchar_t* NString::Proper(wchar_t* temp)
+	{
+		wchar_t* retc = temp;
+
+		int i = 0;
+		while (retc[i])
+		{
+			retc[i] = tolower(retc[i]);
+			i++;
+		}
+		retc[0] = toupper(retc[0]);
+		return retc;
+	}
+
+	std::string NString::ToUpper(std::string temp)
+	{
+		int i = 0;
+		while (temp[i])
+		{
+			temp[i] = toupper(temp[i]);
+			i++;
+		}
+		return temp;
+	}
+
+	std::string NString::ToLower(std::string temp)
+	{
+		int i = 0;
+		while (temp[i])
+		{
+			temp[i] = tolower(temp[i]);
+			i++;
+		}
+		return temp;
+	}
+
+	std::string NString::Proper(std::string temp)
+	{
+		int i = 0;
+		while (temp[i])
+		{
+			temp[i] = tolower(temp[i]);
+			i++;
+		}
+		temp[0] = toupper(temp[0]);
+		return temp;
+	}
+
+	std::wstring NString::ToUpper(std::wstring temp)
+	{
+		int i = 0;
+		while (temp[i])
+		{
+			temp[i] = toupper(temp[i]);
+			i++;
+		}
+		return temp;
+	}
+
+	std::wstring NString::ToLower(std::wstring temp)
+	{
+		int i = 0;
+		while (temp[i])
+		{
+			temp[i] = tolower(temp[i]);
+			i++;
+		}
+		return temp;
+	}
+
+	std::wstring NString::Proper(std::wstring temp)
+	{
+		int i = 0;
+		while (temp[i])
+		{
+			temp[i] = tolower(temp[i]);
+			i++;
+		}
+		temp[0] = toupper(temp[0]);
+		return temp;
+	}
+
+	std::vector<std::string> NString::Split(std::string in_s, const std::string& delim)
+	{
+		std::vector <std::string> retvec;
+		size_t pos = 0;
+		std::string token;
+
+		while ((pos = in_s.find(delim)) != std::string::npos)
+		{
+			token = in_s.substr(0, pos);
+			retvec.push_back(token);
+			in_s.erase(0, pos + delim.length());
+		}
+
+		retvec.push_back(in_s);
+		return retvec;
+	}
+
+	std::vector<std::wstring> NString::Split(std::wstring in_s, const std::wstring & delim)
+	{
+		std::vector <std::wstring> retvec;
+		size_t pos = 0;
+		std::wstring token;
+
+		while ((pos = in_s.find(delim)) != std::wstring::npos)
+		{
+			token = in_s.substr(0, pos);
+			retvec.push_back(token);
+			in_s.erase(0, pos + delim.length());
+		}
+
+		retvec.push_back(in_s);
+		return retvec;
+	}
+
+	std::wstring NString::ToWide(std::string in)
+	{
+		std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+		return converter.from_bytes(in);
+	}
+
+	std::string NString::ToNarrow(std::wstring in)
+	{
+		std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+		return converter.to_bytes(in);
+	}
+}
