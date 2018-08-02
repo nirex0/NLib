11097058daa0be18f406087757db3d3087fa6843
Nirex0 <Nirex.0@gmail.com>
2018-08-03 00:25:06 +0430

    [NLib] Add: NFile utility class

---
+++ src/NCore/NFile.h
@@ -0,0 +1,28 @@
+//© 2018 NIREX ALL RIGHTS RESERVED
+
+#ifndef _W_FILE_H_
+#define _W_FILE_H_
+
+#include "NDefines.h"
+#include <string>
+#include <fstream>
+#include <vector>
+#include <iostream>
+
+namespace NLib
+{
+	class NAPI NFile final
+	{
+	public:
+		std::string ReadAllText(std::string file);
+		NINT WriteAllText(std::string file, std::string str);
+		std::int32_t FindFirst(std::string file, std::string val);
+		
+		std::vector<std::string> ReadAllLines(std::string file);
+		NINT WriteAllLines(std::string file, std::vector<std::string> vecstr);
+		std::vector<int32_t> FindAll(std::string file, std::string val);
+		
+		NBOOL SafeFread(void* buffer, NINT size, NINT number, FILE* fp);
+	};
+}
+#endif // !_W_FILE_H_
