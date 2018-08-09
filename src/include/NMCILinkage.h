18b80075e008f3d24d20ecad609b215a6b3a351c
Nirex0 <Nirex.0@gmail.com>
2018-08-09 13:37:43 +0430

    [NLib] Add: NMCILinkage to the include directory

---
+++ src/include/NMCILinkage.h
@@ -0,0 +1,10 @@
+// © 2018 NIREX ALL RIGHTS RESERVED
+
+#ifndef _N_MCI_LINKAGE_
+#define _N_MCI_LINKAGE_
+
+#include <mmsystem.h>
+#include <mciapi.h>
+#pragma comment(lib, "Winmm.lib")
+
+#endif // !_N_MCI_LINKAGE_
