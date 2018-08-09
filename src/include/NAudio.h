e132a40699e6888a9d29415940275af933b67860
Nirex0 <Nirex.0@gmail.com>
2018-08-09 13:37:31 +0430

    [NLib] Add: NAudio to the include directory

---
+++ src/include/NAudio.h
@@ -0,0 +1,38 @@
+// © 2018 NIREX ALL RIGHTS RESERVED
+
+#ifndef _N_AUDIO_H_
+#define _N_AUDIO_H_
+
+#include "NWindows.h"
+#include "NObject.h"
+#include <string>
+
+#include "NMCILinkage.h" // NOTE: Include after "Windows.h"
+
+namespace NLib
+{
+	class NAudio : public NObject
+	{
+	public:
+		NAudio(const wchar_t* name, const wchar_t* path, HWND handle);
+		~NAudio(void);
+
+		// Getters
+		const wchar_t* Path(void) const;
+		const wchar_t* Name(void) const;
+
+		// Functions
+		void Play(void);
+		void Pause(void);
+		void Resume(void);
+		void Stop(void);
+		void PlayOnRepeat(void);
+
+	private:
+		const wchar_t* m_name;
+		const wchar_t* m_path;
+		HWND m_handle;
+	};
+}
+
+#endif // !_N_AUDIO_H_
