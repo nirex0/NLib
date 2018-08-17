6b175faf3eb15851eb9f6ca54349fd2938550cdf
Nirex0 <Nirex.0@gmail.com>
2018-08-18 02:24:38 +0430

    [NLib] Change: NGenerator's definition

--- src/NCore/NGenerator.h
+++ src/NCore/NGenerator.h
@@ -9,30 +9,35 @@
 
 namespace NLib
 {
-	template <class T>
-	class NGenerator : public NObject
+	class NAPI NGenerator : public NObject
 	{
+	protected:
+		int _line;
 	public:
-		NAPI NGenerator(void)
-		{
-			m_yield = new NRegistry();
-		}
-
-		NAPI ~NGenerator(void)
-		{
-			delete m_yield;
-		}
-
-		NAPI void Generate(T in = {})
-		{
-			NGeneratorArgs<T>* args = new NGeneratorArgs<T>(m_iterationCount, in);
-			m_yield->Run(this, args);
-		}
+		NGenerator() { rewind(); }
+		void rewind() { _line = 0; } // restart the generator
 
 	private:
-		int m_iterationCount;
-		NRegistry* m_yield;
+		NGenerator(const NGenerator&); // non-copyable
+		NGenerator& operator=(const NGenerator&); // non-assignable
 	};
+
+#define $generator(NAME) struct NAME : public generator::instance
+
+#define $emit(T)            bool operator()(T& _rv) { switch(_line) { case 0:;
+#define $emit2(T1,T2)       bool operator()(T1& _rv1, T2& _rv2) { switch(_line) { case 0:;
+#define $emit3(T1,T2,T3)    bool operator()(T1& _rv1, T2& _rv2, T3& _rv3 ) { switch(_line) { case 0:;
+#define $emit4(T1,T2,T3,T4) bool operator()(T1& _rv1, T2& _rv2, T3& _rv3, T4& _rv4 ) { switch(_line) { case 0:;
+
+#define $yield(V)            { _line=__LINE__; _rv = (V); return true; case __LINE__: _line=__LINE__; }
+#define $yield2(V1,V2)       { _line=__LINE__; _rv1 = (V1); _rv2 = (V2); return true; case __LINE__: _line=__LINE__; }
+#define $yield3(V1,V2,V3)    { _line=__LINE__; _rv1 = (V1); _rv2 = (V2); _rv3 = (V3); return true; case __LINE__: _line=__LINE__; }  
+#define $yield4(V1,V2,V3,V4) { _line=__LINE__; _rv1 = (V1); _rv2 = (V2); _rv3 = (V3); _rv4 = (V4); return true; case __LINE__: _line=__LINE__; }
+#define $stop  } _line = 0; return false; }
+
 }
 
 #endif // !_W_GENERATOR_H_
+
+
+	
