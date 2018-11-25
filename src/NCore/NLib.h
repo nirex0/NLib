0fc2481247b461a17acb74115ca6f758a41dd239
Nirex0 <Nirex.0@gmail.com>
2018-11-26 00:18:58 +0330

    Move: EOA Files

---
+++ src/NCore/NLib.h
@@ -0,0 +1,22 @@
+// © 2018 NIREX ALL RIGHTS RESERVED
+
+#ifndef _N_LIB_H_
+#define _N_LIB_H_
+
+#include "NDefines.h"
+#include "NWindows.h"
+
+#include "NObject.h"
+#include "NRegistry.h"
+#include "NGenerator.h"
+#include "NLogicalBound.h"
+#include "NAsyncWorker.h"
+#include "NAudio.h"
+
+#include "NEventArgs.h"
+#include "NAsyncArgs.h"
+
+#include "NFile.h"
+#include "NString.h"
+
+#endif // !_N_LIB_H_
