1d0633031b6168cc6826c605f6aa479396d6c82b
Nirex0 <Nirex.0@gmail.com>
2018-11-29 12:18:12 +0330

    (include) Update: NFile now has SafeFWrite and Byte handling

--- src/include/NFile.h
+++ src/include/NFile.h
@@ -1,7 +1,7 @@
 //© 2018 NIREX ALL RIGHTS RESERVED
 
-#ifndef _W_FILE_H_
-#define _W_FILE_H_
+#ifndef _N_FILE_H_
+#define _N_FILE_H_
 
 #include "NDefines.h"
 #include <string>
@@ -16,13 +16,18 @@ namespace NLib
 	public:
 		std::string ReadAllText(std::string file);
 		NINT WriteAllText(std::string file, std::string str);
-		std::int32_t FindFirst(std::string file, std::string val);
-		
+
 		std::vector<std::string> ReadAllLines(std::string file);
 		NINT WriteAllLines(std::string file, std::vector<std::string> vecstr);
+
+		std::vector<NCHAR> ReadAllBytes(std::string fileName);
+		NINT WriteAllBytes(std::string fileName, std::vector<NCHAR> vecBytes);
+
+		std::int32_t FindFirst(std::string file, std::string val);
 		std::vector<int32_t> FindAll(std::string file, std::string val);
-		
-		NBOOL SafeFread(void* buffer, NINT size, NINT number, FILE* fp);
+
+		NBOOL SafeFread(NPVOID buffer, NINT size, NINT number, FILE* fp);
+		NBOOL SafeFWrite(NPVOID buffer, NINT size, NINT number, FILE* fp);
 	};
 }
-#endif // !_W_FILE_H_
+#endif // !_N_FILE_H_
