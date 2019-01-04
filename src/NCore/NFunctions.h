7125f8699ee812bad3c89fdf7bb3e7ccc129b10d
Nirex0 <Nirex.0@gmail.com>
2019-01-04 15:55:30 +0330

    Create NFunctions.h

---
+++ src/NCore/NFunctions.h
@@ -0,0 +1,23 @@
+// © 2018 NIREX ALL RIGHTS RESERVED
+
+#ifndef _N_FUNCTIONS_H_
+#define _N_FUNCTIONS_H_
+
+#include <functional>
+#include "NDefines.h"
+
+
+#include "NObject.h"
+#include "NEventArgs.h"
+
+namespace NLib
+{
+	typedef NAPI std::function<void(void)> BaseAnnex;
+
+	typedef NAPI std::function<void(bool&)> BindAnnex;
+
+	typedef NAPI std::function<void(NObject* sender, NEventArgs* args)> RegistryAnnex;
+}
+
+
+#endif // !_N_FUNCTIONS_H_
