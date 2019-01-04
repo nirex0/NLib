94a3aa5f915b96b25d173b5edb52454cfea7eb08
Nirex0 <Nirex.0@gmail.com>
2019-01-04 15:15:12 +0330

    Add: Perfect chance class

---
+++ src/NCore/NPerfectChance.cpp
@@ -0,0 +1,105 @@
+// © 2018 NIREX ALL RIGHTS RESERVED
+
+#include "NPerfectChance.h"
+#include <math.h>
+
+namespace NLib
+{
+	NPerfectChance::NPerfectChance(const size_t& pct_chance)
+		: iterator(0)
+		, proc(0)
+	{
+		if (pct_chance > 100)
+		{
+			chance = 100;
+			proc = (size_t)roundf(float(100 / chance));
+			return;
+		}
+		chance = pct_chance;
+		proc = (size_t)roundf(float(100 / chance));
+	}
+
+	size_t NPerfectChance::GetChance(void) const
+	{
+		return chance;
+	}
+
+	void NPerfectChance::SetChance(size_t value)
+	{
+		if(chance == value)
+		{
+			return;
+		}
+
+		if (value > 100)
+		{
+			chance = 100;
+			proc = (size_t)roundf(float(100 / chance));
+		}
+		else
+		{
+			chance = value;
+			proc = (size_t)roundf(float(100 / chance));
+		}
+
+		iterator = 0;
+	}
+
+	size_t NPerfectChance::GetProcValue(void) const
+	{
+		return proc;
+	}
+
+	size_t NPerfectChance::GetIterator(void) const
+	{
+		return iterator;
+	}
+
+	void NPerfectChance::Reset(void)
+	{
+		iterator = 0;
+		proc = (size_t)roundf(float(100 / chance));
+	}
+
+	bool NPerfectChance::operator()(void)
+	{
+		if (iterator == proc)
+		{
+			iterator = 0;
+			return true;
+		}
+		else
+		{
+			iterator++;
+			return false;
+		}
+	}
+
+	bool NPerfectChance::Get(void)
+	{
+		if (iterator == proc)
+		{
+			iterator = 0;
+			return true;
+		}
+		else
+		{
+			iterator++;
+			return false;
+		}
+	}
+
+	bool NPerfectChance::Perform(void)
+	{
+		if (iterator == proc)
+		{
+			iterator = 0;
+			return true;
+		}
+		else
+		{
+			iterator++;
+			return false;
+		}
+	}
+}
---
+++ src/NCore/NPerfectChance.h
@@ -0,0 +1,34 @@
+// © 2018 NIREX ALL RIGHTS RESERVED
+
+#ifndef _N_PERFECT_CHANCE_H_
+#define _N_PERFECT_CHANCE_H_
+
+#include "NDefines.h"
+
+namespace NLib
+{
+	class NAPI NPerfectChance
+	{
+	public:
+		NPerfectChance(const size_t& pct_chance = 50);
+
+		size_t GetChance(void) const;
+		void SetChance(size_t value);
+
+		size_t GetProcValue(void) const;
+		size_t GetIterator(void) const;
+
+		void Reset(void);
+
+		bool operator()(void);
+		bool Get(void);
+		bool Perform(void);
+
+	private:
+		size_t chance;
+		size_t iterator;
+		size_t proc;
+	};
+}
+
+#endif // _N_PERFECT_CHANCE_H_
