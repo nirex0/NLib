1b885454970f4939927102eab79283ae09558cb8
Nirex0 <Nirex.0@gmail.com>
2019-01-19 15:07:07 +0330

    Create NRegexFlags.h

---
+++ src/NCore/NRegexFlags.h
@@ -0,0 +1,18 @@
+// © 2018 NIREX ALL RIGHTS RESERVED
+
+#ifndef _N_REGEX_FLAGS_H_
+#define _N_REGEX_FLAGS_H_
+
+#include "NDefines.h"
+
+namespace NLib
+{
+	enum NRegexFlags
+	{
+		RXF_GLOBAL = 1,
+		RXF_MULTILINE = 2,
+		RXF_CASEINSENSITIVE = 4
+	};
+}
+
+#endif // !_N_REGEX_FLAGS_H_
