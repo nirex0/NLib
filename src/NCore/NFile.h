39fab61570fe8eab2b1715e615b74825fe5aaa89
Nirex0 <Nirex.0@gmail.com>
2019-01-28 23:36:53 +0330

    Update: NFile methods are now all static

--- src/NCore/NFile.h
+++ src/NCore/NFile.h
@@ -14,20 +14,20 @@ namespace NLib
 	class NAPI NFile final
 	{
 	public:
-		std::string ReadAllText(std::string file);
-		NINT WriteAllText(std::string file, std::string str);
+		static std::string ReadAllText(std::string file);
+		static NINT WriteAllText(std::string file, std::string str);
 
-		std::vector<std::string> ReadAllLines(std::string file);
-		NINT WriteAllLines(std::string file, std::vector<std::string> vecstr);
+		static std::vector<std::string> ReadAllLines(std::string file);
+		static NINT WriteAllLines(std::string file, std::vector<std::string> vecstr);
 
-		std::vector<NCHAR> ReadAllBytes(std::string fileName);
-		NINT WriteAllBytes(std::string fileName, std::vector<NCHAR> vecBytes);
+		static std::vector<NCHAR> ReadAllBytes(std::string fileName);
+		static NINT WriteAllBytes(std::string fileName, std::vector<NCHAR> vecBytes);
 
-		std::int32_t FindFirst(std::string file, std::string val);
-		std::vector<int32_t> FindAll(std::string file, std::string val);
+		static std::int32_t FindFirst(std::string file, std::string val);
+		static std::vector<int32_t> FindAll(std::string file, std::string val);
 
-		NBOOL SafeFread(NPVOID buffer, NINT size, NINT number, FILE* fp);
-		NBOOL SafeFWrite(NPVOID buffer, NINT size, NINT number, FILE* fp);
+		static NBOOL SafeFread(NPVOID buffer, NINT size, NINT number, FILE* fp);
+		static NBOOL SafeFWrite(NPVOID buffer, NINT size, NINT number, FILE* fp);
 	};
 }
 #endif // !_N_FILE_H_
