91b25feaaebacc3f7e4f5a2e9d487c87cffacac5
Nirex0 <Nirex.0@gmail.com>
2019-02-04 23:16:08 +0330

    Optimize: Some of the NFile Methods

--- src/NCore/NFile.h
+++ src/NCore/NFile.h
@@ -15,17 +15,17 @@ namespace NLib
 	class NAPI NFile final
 	{
 	public:
-		static std::string ReadAllText(std::string file);
-		static NINT WriteAllText(std::string file, std::string str);
+		static std::string ReadAllText(const std::string& file);
+		static NINT WriteAllText(const std::string& file, const std::string& str);
 
-		static std::vector<std::string> ReadAllLines(std::string file);
-		static NINT WriteAllLines(std::string file, std::vector<std::string> vecstr);
+		static std::vector<std::string> ReadAllLines(const std::string& file);
+		static NINT WriteAllLines(const std::string& file, std::vector<std::string> vecstr);
 
-		static std::vector<NCHAR> ReadAllBytes(std::string fileName);
-		static NINT WriteAllBytes(std::string fileName, std::vector<NCHAR> vecBytes);
+		static std::vector<NCHAR> ReadAllBytes(const std::string& fileName);
+		static NINT WriteAllBytes(const std::string& fileName, std::vector<NCHAR> vecBytes);
 
-		static std::int32_t FindFirst(std::string file, std::string val);
-		static std::vector<int32_t> FindAll(std::string file, std::string val);
+		static std::int32_t FindFirst(const std::string& file, const std::string& val);
+		static std::vector<int32_t> FindAll(const std::string& file, const std::string& val);
 
 		static NBOOL SafeFRead(NPVOID buffer, NINT size, NINT number, FILE* fp);
 		static NBOOL SafeFWrite(NPVOID buffer, NINT size, NINT number, FILE* fp);
